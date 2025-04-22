7915471d8417fc26c61d448f5c7c1cf49b36742f
treeofmapple <samuelsantana778@gmail.com>
2025-04-22 15:05:08 -0300

    minor change

--- ShortURL/src/main/java/com/tom/service/shortener/config/SecurityConfig.java
+++ ShortURL/src/main/java/com/tom/service/shortener/config/SecurityConfig.java
@@ -1,6 +1,6 @@
-	package com.tom.service.shortener.config;
+package com.tom.service.shortener.config;
 	
-	import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -19,44 +19,44 @@ import com.tom.service.shortener.repository.RedisPersistentTokenRepository;
 
 import lombok.RequiredArgsConstructor;
 	
-	@Configuration
-	@EnableWebSecurity
-	@EnableMethodSecurity
-	@RequiredArgsConstructor
-	public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
 	
-		@Value("${application.security.jwt.secret-key}")
-		private String secretKey;
+	@Value("${application.security.jwt.secret-key}")
+	private String secretKey;
 	
-		private final AuthEntryPointJwt unauthorizedHandler;
+	private final AuthEntryPointJwt unauthorizedHandler;
 	
-		private final RedisTemplate<String, Object> redisTemplate;
+	private final RedisTemplate<String, Object> redisTemplate;
 	
-		private final UserDetailsService userDetailsService;
-		
-		@Bean
-		SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-			http.csrf(AbstractHttpConfigurer::disable)
-					.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-					.authorizeHttpRequests(auth -> auth
-							.requestMatchers("/actuator/**").authenticated()
-							.requestMatchers("/api/v1/dev/**").authenticated()
-							.requestMatchers("/api/v1/**").permitAll()
-							.requestMatchers("/**").permitAll()
-							.anyRequest().permitAll()
-							)
-					.rememberMe(rememberMe -> rememberMe
-							.tokenValiditySeconds(1800)
-							.key(secretKey)
-							.userDetailsService(userDetailsService)
-							.tokenRepository(persistentTokenRepository(redisTemplate)))
-					.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
-					.httpBasic(Customizer.withDefaults());
-			return http.build();
-		}
+	private final UserDetailsService userDetailsService;
 	
-		@Bean
-		PersistentTokenRepository persistentTokenRepository(RedisTemplate<String, Object> redisTemplate) {
-			return new RedisPersistentTokenRepository(redisTemplate);
-		}
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+				.exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/actuator/**").authenticated()
+						.requestMatchers("/api/v1/dev/**").authenticated()
+						.requestMatchers("/api/v1/**").permitAll()
+						.requestMatchers("/**").permitAll()
+						.anyRequest().permitAll()
+						)
+				.rememberMe(rememberMe -> rememberMe
+						.tokenValiditySeconds(1800)
+						.key(secretKey)
+						.userDetailsService(userDetailsService)
+						.tokenRepository(persistentTokenRepository(redisTemplate)))
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+				.httpBasic(Customizer.withDefaults());
+		return http.build();
 	}
+
+	@Bean
+	PersistentTokenRepository persistentTokenRepository(RedisTemplate<String, Object> redisTemplate) {
+		return new RedisPersistentTokenRepository(redisTemplate);
+	}
+}
