a075b6471026c120efb89919303c7931975f1b86
treeofmapple <samuelsantana778@gmail.com>
2025-09-30 20:20:13 -0300

    Large update on system
    - Test implementation
    - Clearing the code
    - Making it more readable
    - Making it more reusable
    - Later implementation on front end
    - Better Testing
    - Less Boilerplate

--- ShortURL/src/main/java/com/tom/service/shortener/exception/CustomGlobalException.java
+++
@@ -1,6 +0,0 @@
-package com.tom.service.shortener.exception;
-
-@SuppressWarnings("serial")
-public abstract class CustomGlobalException extends RuntimeException {
-	public abstract String getMsg();
-}
--- ShortURL/src/main/java/com/tom/service/shortener/exception/ErrorResponse.java
+++
@@ -1,6 +0,0 @@
-package com.tom.service.shortener.exception;
-
-import java.util.Map;
-
-public record ErrorResponse(Map<String, String> errors) {
-}
